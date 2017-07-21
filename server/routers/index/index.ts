b563a656361d2b16c773ed6ed8963dbcb73adfa6
WoLfulus <wolfulus@gmail.com>
2017-07-21 18:36:03 -0300

    Added example dependency injection and settings retrieval

--- server/routers/index/index.ts
+++ server/routers/index/index.ts
@@ -1,14 +1,39 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 
 import { get, router } from "../../core/http/decorators";
 import { IRequest, IResponse, Router } from "../../core/http/router";
+import { ConfigServiceId, IConfigService } from "../../services/config/interface";
+
+/**
+ * Response interface
+ */
+interface IVersionResponse {
+    commit: string;
+    environment: string;
+    version: string;
+    something: string;
+}
 
 @injectable()
 @router()
 export class IndexRouter extends Router {
 
+    @inject(ConfigServiceId)
+    private config: IConfigService;
+
     @get("/")
     public async index(request: IRequest, response: IResponse): Promise<string> {
         return "apito";
     }
+
+    @get("/version")
+    public async version(request: IRequest, response: IResponse): Promise<IVersionResponse> {
+        return {
+            commit: this.config.commit,
+            environment: this.config.environment,
+            something: this.config.options.this.is.something,
+            version: this.config.version,
+        };
+    }
+
 }
